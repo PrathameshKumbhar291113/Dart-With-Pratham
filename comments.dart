2f5725ef26dab3654aff4c1270552551114ea542
Prathamesh Kumbhar <prathameshkumbhar291113@gmail.com>
2022-09-23 23:01:42 +0530

    [Started Dart] 1. Learned the syntax of the dart. 2. Learned the commenting in the dart.

---
+++ comments.dart
@@ -0,0 +1,5 @@
+//In this code we will do the dart comments
+
+/*
+this is the multiple line comments
+ */
---
+++ hello.dart
@@ -0,0 +1,13 @@
+ void main(){
+  print("hello world");
+  print("This is my first application");
+  print(4);
+  print(4-1);
+  print(12/4);
+
+  if(3==2){
+    print("This is true");
+  }else{
+    print("This is not true");
+  }
+ }
